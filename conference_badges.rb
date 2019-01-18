338049b3591e7fe3ef7732b5958e386faaf9fcb9
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-18 04:35:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,6 @@ end
 
 def assign_rooms(attendees)
   attendees.collect do |name|
-    "Hello, ${name}! You'll be assigned to room 1!"
+    "Hello, #{name}! You'll be assigned to room !"
   end
 end
